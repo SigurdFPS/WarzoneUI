b0aa3eaaa0050fe68f497da4a5c39e83e8134657
SigurdFPS <rigginscody@gmail.com>
2024-08-04 21:08:18 -0500

    Create API.js

---
+++ Integration/API.js
@@ -0,0 +1,6 @@
+// Use Twitch.js to get the streamer's info and display loadouts accordingly.
+const twitch = window.Twitch.ext;
+
+twitch.onAuthorized(auth => {
+  // Use auth token to interact with Twitch API if needed
+});
